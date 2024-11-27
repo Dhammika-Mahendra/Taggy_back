44645b3c6ee1cb2d2015d4a22d6979832da86652
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-11-27 11:02:48 +0530

    link metadata service temporary removed

--- src/links/links.service.ts
+++ src/links/links.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { GetLinksArgs } from './dto/args/get-links-args.dto';
-import { getLinkPreview } from 'link-preview-js';
 
 @Injectable()
 export class LinksService {
   async getLinks(getLinksArgs: GetLinksArgs) {
-    return Promise.all(
-      getLinksArgs.urls.map(async (url) => {
-        return getLinkPreview(url);
-      }),
-    );
+    return getLinksArgs.urls.map((url) => ({
+      title: url,
+      siteName: null,
+      url: url,
+      images: []
+    }));
   }
 }
